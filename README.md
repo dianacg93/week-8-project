e356e9872220be60f415a5a6cc21133a3e315bc0
DianaG <diana.cg93@gmail.com>
2020-11-12 23:05:00 -0500

    store and product created

---
+++ README.md
@@ -0,0 +1,7 @@
+For this week’s project assignment, the class will be tasked with creating a application which manages an inventory of products. 
+The class must create a product class, which has a price id, and quantity on hand. 
+Then create an inventory class which keeps track of various products. 
+
+In regards to the CSS, for now we won’t place too much important into that. The results can be shown on the console. 
+
+Plus- If you do finish early. To implement this code with a responsive user interface. To show the results, Bootstrap is recommended. 
--- index.js
+++ index.js
@@ -16,32 +16,49 @@ class Inventory {
     }
 
     addItem(product){
-        if(!this.items[product]){
-            this.items[product] = product;
+        if(!this.items[product.id]){
+            this.items[product.id] = product;
         }else {
             console.log('Product already in inventory')
         }
     }
 
-    addQuantity(product, quantity){
-        if(!this.items[product]){
-            product[quantity]++;
+    removeItem(product){
+        if(this.items[product.id]){
+            delete this.items[product.id];
+        }else {
+            console.log("Product does not exist in inventory")
         }
     }
 
     printInventory(){
-        console.log( this.items);
+        if(Object.keys(this.items).length == 0){
+            console.log('the inventory is empty');
+        }else {
+            console.log(this.items);
+        }
     }
 }
 
 const table = new Product(25, 1, 3);
-// table.printItem()
-console.log(table)
 const chair = new Product(18, 2, 5);
+const umbrella = new Product(3, 3, 10);
+const pen = new Product(2, 4, 50);
+const headphones = new Product(40, 5, 5);
 
-const store = new Inventory()
-// store.printInventory()
+const store = new Inventory();
+
+store.printInventory()
 store.addItem(table)
 store.printInventory()
 store.addItem(chair)
-store.printInventorgity()
+store.printInventory()
+store.removeItem("book")
+store.addItem(umbrella)
+store.printInventory();
+store.addItem(pen)
+store.printInventory();
+store.addItem(headphones)
+store.printInventory();
+store.removeItem(headphones)
+store.printInventory();
